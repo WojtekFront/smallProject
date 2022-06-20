5d57f7cd26c33820001792eaf66beee57fd0a02d
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2022-06-20 23:03:25 +0200

    first commit calculator
    calculator project without logic in JS

---
+++ Calculator/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="calculator">
+    
+                <div id="result" colspan="5">value</div>
+       <div id="buttons">
+            <!-- keybord -->
+            
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>+</button>
+                <button>-</button>
+            
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>*</button>
+                <button>/</button>
+          
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>%</button>
+                <button>^</button>
+            
+                <button>.</button>
+                <button>0</button>
+                <button class="spanthree">=</button>
+                <!-- close buttons  -->
+       </div>
+      
+    <!-- close calculator  -->
+</div>
+    
+</body>
+<script src="js/button.js"></script>
+
+</html>
