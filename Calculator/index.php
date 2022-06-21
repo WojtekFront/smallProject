2f13df7e9da13c8c218a5a6209b50df502a33928
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2022-06-21 23:41:14 +0200

    looks better
    few correct in design

--- Calculator/index.php
+++ Calculator/index.php
@@ -11,38 +11,42 @@
 
 <body>
     <div id="calculator">
-    
-                <div id="result" colspan="5">value</div>
-       <div id="buttons">
-            <!-- keybord -->
-            
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>+</button>
-                <button>-</button>
-            
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>*</button>
-                <button>/</button>
-          
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>%</button>
-                <button>^</button>
-            
-                <button>.</button>
-                <button>0</button>
-                <button class="spanthree">=</button>
-                <!-- close buttons  -->
-       </div>
-      
-    <!-- close calculator  -->
-</div>
-    
+
+        <div id="result" colspan="5">
+            <div data-previous-operand id="first-data">222</div>
+            <div data-current-operand id="second-data">2222333</div>
+
+        </div>
+
+        <!-- keybord -->
+
+        <button data-number>1</button>
+        <button data-number>2</button>
+        <button data-number>3</button>
+        <button data-operation>+</button>
+        <button data-operation>-</button>
+
+        <button data-number>4</button>
+        <button data-number>5</button>
+        <button data-number>6</button>
+        <button data-operation>*</button>
+        <button data-operation>/</button>
+
+        <button data-number>7</button>
+        <button data-number>8</button>
+        <button data-number>9</button>
+        <button data-operation>%</button>
+        <button data-operation>^</button>
+
+        <button data-number>.</button>
+        <button data-number>0</button>
+        <button data-equals class="spanthree">=</button>
+        <!-- close buttons  -->
+
+
+        <!-- close calculator  -->
+    </div>
+
 </body>
 <script src="js/button.js"></script>
 
