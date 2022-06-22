2a59be0d2099b036b673e886cc5108860618e1ba
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2022-06-22 23:51:12 +0200

    add JS
    add logic in JS

--- Calculator/JS/button.js
+++ Calculator/JS/button.js
@@ -1,1 +1,75 @@
-document.getElementById("result").innerHTML=7;
+class Calculator{
+constructor(dataFirst,dataSecond){
+    this.dataFirst=dataFirst;
+    this.dataSecond=dataSecond;
+    this.clear();
+
+}
+clear(){
+this.firstData="";
+this.secondData="";
+this.operation=undefined;
+}
+
+delete(){
+
+}
+
+appendNumber(number){
+    if(number ==="." && this.secondData.includes('.')) return
+this.secondData=this.secondData.toString()+ number.toString();
+}
+
+choseOperation(operation){
+    if(this.secondData="") return
+    if(this.firstData!=""){
+this.compute();
+    } 
+this.operation = operation;
+this.firstData = this.secondData;
+this.secondData = '';
+}
+
+compute(){
+
+}
+
+updateDisplay(button){
+this.dataSecond.innerText = this.secondData;
+this.dataFirst.innerText = this.firstData;
+}
+}
+
+const numberButtons = document.querySelectorAll('[buttonNumber');
+const operationButtons = document.querySelectorAll('[buttonSign]');
+const equalButton = document.querySelector('[buttonEquals]');
+const buttonClear = document.querySelector('[buttonClear]');
+const buttonDelete = document.querySelector('[buttonDelete]');
+const dataFirst = document.querySelector('[data-first-value]');
+const dataSecond = document.querySelector('[data-second-value');
+
+const calculator = new Calculator(dataFirst, dataSecond );
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        calculator.choseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+    
+})
+
+
+
+
+
+
+
+
+
--- Calculator/index.php
+++ Calculator/index.php
@@ -10,40 +10,37 @@
 </head>
 
 <body>
-    <div id="calculator">
+    <div class="calculator">
 
-        <div id="result" colspan="5">
-            <div data-previous-operand id="first-data">222</div>
-            <div data-current-operand id="second-data">2222333</div>
-
+        <div class="result" >
+            <div data-first-value class="firstData"></div>
+            <div data-second-value class="secondData"></div>
         </div>
 
         <!-- keybord -->
+        <button buttonNumber>1</button>
+        <button buttonNumber>2</button>
+        <button buttonNumber>3</button>
+        <button buttonClear>DEL</button>
+        <button buttonDelete><<</button>
 
-        <button data-number>1</button>
-        <button data-number>2</button>
-        <button data-number>3</button>
-        <button data-operation>+</button>
-        <button data-operation>-</button>
+        <button buttonNumber>4</button>
+        <button buttonNumber>5</button>
+        <button buttonNumber>6</button>
+        <button buttonSign>+</button>
+        <button buttonSign>-</button>
 
-        <button data-number>4</button>
-        <button data-number>5</button>
-        <button data-number>6</button>
-        <button data-operation>*</button>
-        <button data-operation>/</button>
+        <button buttonNumber>7</button>
+        <button buttonNumber>8</button>
+        <button buttonNumber>9</button>
+        <button buttonSign>*</button>
+        <button buttonSign>/</button>
 
-        <button data-number>7</button>
-        <button data-number>8</button>
-        <button data-number>9</button>
-        <button data-operation>%</button>
-        <button data-operation>^</button>
-
-        <button data-number>.</button>
-        <button data-number>0</button>
-        <button data-equals class="spanthree">=</button>
-        <!-- close buttons  -->
-
-
+        <button buttonNumber>.</button>
+        <button buttonNumber>0</button>
+        <button buttonEquals>=</button>
+        <button buttonSign>%</button>
+        <button buttonSign>^</button>
         <!-- close calculator  -->
     </div>
 
