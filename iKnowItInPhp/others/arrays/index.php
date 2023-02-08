515d15374e780b20b7e3f6883b2edbc9668a1b73
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2023-02-08 10:07:00 +0100

    add recursion

--- iKnowItInPhp/others/arrays/index.php
+++ iKnowItInPhp/others/arrays/index.php
@@ -12,8 +12,10 @@
         <?php
             $bookLists= [
                 ['titleA'=>'Show biggest value from array', 'describeA'=>'select the greatest value from the array = [1, 10, 2, 3, 4, 5, 6, 7, 8, 9, 0]', 'fileA'=>'biggestValue'],
-                
+                ['titleA'=>'Recursion', 'describeA'=>'In this exercise I will take the value and add to it. I start from 1 and finish below 1000', 'fileA'=>'recursionValue'],
+                ['titleA'=>'Test', 'describeA'=>'testing area', 'fileA'=>'sample'],
                 ['titleA'=>'', 'describeA'=>'', 'fileA'=>''],
+                ['titleA'=>'', 'describeA'=>'', 'fileA'=>'']
             ];
             foreach($bookLists as $book){
                 if(!empty($book['titleA']) && !empty($book['describeA']) && !empty($book['fileA']))
