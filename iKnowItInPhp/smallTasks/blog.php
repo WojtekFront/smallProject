d4a4e32368bb6d7a2eaab15cc75075e80eb79546
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2023-02-25 16:17:36 +0100

    create new post

---
+++ iKnowItInPhp/smallTasks/blog.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+<table>
+<thead>
+<tr>
+    <th style="width: 3%;">LP</th>
+    <th style="width: 16%;">Autor</th>
+    <th style="width: 70%;">Text</th>
+    <th style="width: 5%;">Date</th>
+    <th style="width: 6%;"></th>
+</tr>
+</thead>
+<tbody>
+<tr>
+    <td>0</td>
+    <td>Autor1</td>
+    <td>To jest pierwszy post</td>
+    <td><?php echo date("y-m-d"); ?></td>
+    <td><button>EDYTUJ</button><button>DODAJ</button><button>USUŃ</button>
+</tr>
+</tbody>
+<caption>Lista postów</caption>
+</table>
+
+<s><br> 1. stworzenie okna z 10 postami oraz dodanie przycisku tytuł + autor, tekst, przyciski "EDYTUJ", "DODAJ", "USUŃ". W pierwszej wersji będzie to tabela.</s>
+<br> 2. 
+<br> 3.
+<br> 4.
+<br> 5.
+<br> 6.
+<br> 7.
+<br> 8.
+<br> 9.    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
