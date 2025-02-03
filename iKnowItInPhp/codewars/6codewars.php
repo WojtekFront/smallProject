302c74005348b45bfb494c61d9f6e293ad81038b
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2025-02-03 15:06:03 +0100

    codewars another functions

---
+++ iKnowItInPhp/codewars/6codewars.php
@@ -0,0 +1,16 @@
+<!-- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. -->
+
+<?php
+
+function even_or_odd(int $n): string {
+  
+    return $n%2 ? "Odd" : "Even"; // Your code here
+}
+?>
+<!-- Given a random non-negative number, you have to return the digits of this number within an array in reverse order. -->
+
+<?php
+function digitize(int $n): array{
+    return array_reverse( str_split((string)$n) );
+  }
+  ?>
